fed99374768d1ef34fcc536c2e4d3b747f3f67ae
Rishi Latchmepersad <latchmepersad@gmail.com>
2019-12-19 11:16:39 -0400

    Create python3_template.py

---
+++ python3_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+"""This is the docstring. It should be a description of your Python script/module"""
+ 
+# Put all your imports here, one per line. However multiple imports from the same lib are allowed on a line.
+ 
+# Put your constants here. These should be named in CAPS.
+
+# Put your global variables here. 
+ 
+# Put your class definitions here. These should use the CapWords convention.
+ 
+# Put your function definitions here. These should be lowercase, separated by underscores.
+ 
+def main():
+	"""Launcher."""
+	# init the GUI or anything else
+	pass
+ 
+if __name__ == "__main__":
+	main()
